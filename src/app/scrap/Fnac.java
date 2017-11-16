09b1d79188e0840d836860818f4d96f76dec6282
Maria <macode1441@gmail.com>
2017-11-16 16:15:05 +0100

    [WRAPPER] FNAC - MAS COSAS

--- src/app/scrap/Fnac.java
+++ src/app/scrap/Fnac.java
@@ -1,5 +1,6 @@
 package app.scrap;
 
+import app.model.Categoria;
 import app.model.Web;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -16,15 +17,18 @@ import java.util.List;
 public class Fnac {
 
     public static void main(String[] args) {
-        Driver();
+        ExeDriver();
     }
 
+    public static final String URL_FNAC = "http://fnac.es";
+
     //WebDriver
-    public static void Driver()
-    {
+    public static void ExeDriver() {
         String exePath = "C:\\Program Files (x86)\\geckodriver-v0.19.1-win64\\geckodriver.exe";
         System.setProperty("webdriver.gecko.driver", exePath);
-        String URL = "http://fnac.es";
+    }
+
+    public List<Categoria> TiposCafetera(){
 
         try {
 
@@ -33,18 +37,18 @@ public class Fnac {
             //Crear Driver
             WebDriver driver = new FirefoxDriver(capabilities);
             //Abrir url
-            driver.get(URL);
+            driver.get(URL_FNAC);
 
             driver.findElement(By.cssSelector("i[class*='close']")).click();
 
-            WebDriverWait waiting = new WebDriverWait(driver,5);
+            WebDriverWait waiting = new WebDriverWait(driver, 5);
 
             WebElement menu_hogar = driver.findElement(By.cssSelector("a[href*='https://www.fnac.es/hogar#bl=MMHogar']"));
 
             menu_hogar.click();
             menu_hogar.click();
 
-            waiting = new WebDriverWait(driver,5);
+            waiting = new WebDriverWait(driver, 5);
             /*waiting.until(
                     ExpectedConditions.presenceOfElementLocated(
                             By.cssSelector("a[href*='https://www.fnac.es/Desayuno-y-cafe/s38477#bl=MMHogar']")
@@ -60,82 +64,20 @@ public class Fnac {
             menos2.click();
 
             List<WebElement> tipos_cafetera = menu_lat.findElements(By.className("categoryMenu-link"));
+            /*List<Categoria> categorias =
 
-            for(WebElement cafetera : tipos_cafetera) {
-                if(!cafetera.getText().equals(""))
-                {
-                    /* Crear tipo cafetera en "lista de tipos"*/
-                    System.out.println("TIPO : "+cafetera.getText());
-
+            for (WebElement tipo : tipos_cafetera) {
+                if (!tipo.getText().equals("")) {
+                   categorias.add(tipo);
                 }
-            }
-
+            }*/
+            
             //driver.quit();
-        }catch(Exception exc)
-        {
+        } catch (Exception exc) {
             System.out.print(exc);
         }
-
-        /** PASO 1 - Buscar href cafeteras y pulsarlo**/
-
-/*
-        WebElement home =  driver.findElement(By.xpath("//a[@href='/?level=10']"));
-        try{
-
-            WebElement burguer = driver.findElement(By.id("burguer"));
-            //WebElement burguer = driver.findElement(By.cssSelector("a[id*='burguer'] "));
-            //WebElement burguer = driver.findElement(By.cssSelector("span[contains(text(), 'burguer icon')]"));
-            //WebElement burguer = driver.findElement(By.cssSelector("span[class='burguer.icon']"));
-            burguer.click();
-            //Error por los espacios
-            //WebElement search = driver.findElement(By.className("burguer icon"));
-            WebDriverWait waiting = new WebDriverWait(driver, 50);
-
-        }catch(Exception exc)
-        {
-            System.out.print(exc);
-        }
-
-        /*
-        WebElement burguerMenu = driver.findElement(By.className("burguer icon"));
-        WebDriverWait waiting = new WebDriverWait(driver, 10);
-        burguerMenu.click();
-
-        WebElement cafeteras =  driver.findElement(By.xpath("//a[@href='/electrodomesticos/cafeteras/?level=1']"));
-        cafeteras.click();
-
-
-*/
-        /** PASO 2 - Esperar **/
-
-        //waiting = new WebDriverWait(driver, 4);
-
-
-
-        /** PASO 3 - Buscar elemento **/
-        /** PASO 4 - Cerrar la ventana de cookies **/
-        /** PASO 5 - **/
-        /** Pulsar Elementos **/
+    }
 
 
     }
-
-    /** Esperar a que se cargue **/
-
-    /*
-    public static void waitForPageLoad(WebDriver wdriver){
-        WebDriverWait wait = new WebDriverWait(wdriver, 60);
-        Predicate<WebDriver> pageLoaded = new Predicate<WebDriver>() {
-            @Override
-            public boolean apply(WebDriver input) {
-                return (
-                        (JavascriptExecutor) input)
-                        .executeScript("return document.readyState").equals("complete");
-            };
-
-        };
-        wait.until(pageLoaded);
-    }
-    */
-
 }
